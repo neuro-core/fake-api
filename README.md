64124962e303be8f3c047b13f8312217c55392e9
lgg <fed777os@gmail.com>
2020-06-24 02:16:57 +0300

    add handling POST requests as GET requests

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Some fake apis for debug and dev
 ```
 PORT=23700 - Active port
 ROUTES=api/db.json - path to active JSON API
+REWRITE_POST_AS_GET=true - handle POST requests as GET requests
 ```
 
 ## License
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const ROUTES = process.env.ROUTES,
     PORT = process.env.PORT;
+let rewritePostAsGet = false;
+try {
+    rewritePostAsGet = process.env.REWRITE_POST_AS_GET ? JSON.parse(process.env.REWRITE_POST_AS_GET) : false;
+} catch (e) {
+
+}
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
@@ -9,6 +15,24 @@ const router = jsonServer.router(ROUTES);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+//######################################
+
+if (rewritePostAsGet){
+    server.use(jsonServer.bodyParser);
+
+    server.use(function (req, res, next) {
+        if (req.method === 'POST') {
+            // Converts POST to GET and move payload to query params
+            // This way it will make JSON Server that it's GET request
+            req.method = 'GET';
+            req.query = req.body
+        }
+        // Continue to JSON Server router
+        next()
+    });
+}
+
+//######################################
 server.use(router);
 server.listen(PORT, () => {
     console.log(`JSON Server is running on: http://localhost:${PORT}`)
